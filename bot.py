bc87a2850f94f4d1b81d0a5acaf8b5fceb558bf6
Miyras1998 <isakovmiyrastarasovich@gmail.com>
2026-05-12 18:03:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-
-from config import 8615206121:AAEpg_mR6nMMijVH5IoIB-Z3EZ7Ob2kSPpA
+from config import BOT_TOKEN
 from database import init_db
 import common
 import admin
@@ -13,22 +12,16 @@ import passenger
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-
 async def main():
     await init_db()
-
-    bot = Bot(token=8615206121:8615206121:AAEpg_mR6nMMijVH5IoIB-Z3EZ7Ob2kSPpA)
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
-
-    # Routers
     dp.include_router(common.router)
     dp.include_router(admin.router)
     dp.include_router(driver.router)
     dp.include_router(passenger.router)
-
     logger.info("🚖 JolawshiBot ishga tushdi!")
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-
 
 if __name__ == "__main__":
     asyncio.run(main())
