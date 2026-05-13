393bcf963525c30e93bfc6c12561259894683a19
Miyras1998 <isakovmiyrastarasovich@gmail.com>
2026-05-13 15:54:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8615206121:AAEpg_mR6nMMijVH5IoIB-Z3EZ7Ob2kSPpA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8615206121:AAF_YysY5PiPfhKTtX5F9AnIr11kNbhIhCU")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5103980244").split(",")))
 CHANNEL_ID = os.getenv("CHANNEL_ID", "@Jolawshi_bot_buyirtpa")
 DB_PATH = os.getenv("DB_PATH", "jolawshi.db")
